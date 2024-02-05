fc3c140f290dc36b5d2f0c1c7eef0eccd4c26266
Robert Sainton <robertsainton@outlook.com>
2024-02-05 01:35:10 -0500

    Updates

---
+++ precache-manifest.d3a90092be2bc437248a853df965065c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b70916b7f149e02292f",
+    "url": "/portfolio/static/css/main.a1a1b611.chunk.css"
+  },
+  {
+    "revision": "8b70916b7f149e02292f",
+    "url": "/portfolio/static/js/main.ff1b5be1.chunk.js"
+  },
+  {
+    "revision": "2823c1b008fae9954d18",
+    "url": "/portfolio/static/js/runtime~main.3945e777.js"
+  },
+  {
+    "revision": "2bce2d1a9716954dd12f",
+    "url": "/portfolio/static/js/2.acd0415e.chunk.js"
+  },
+  {
+    "revision": "cf634016a89517c96401",
+    "url": "/portfolio/static/js/3.5900c917.chunk.js"
+  },
+  {
+    "revision": "bc5f3f876997aab191ca7ca699c278ce",
+    "url": "/portfolio/static/media/js.bc5f3f87.png"
+  },
+  {
+    "revision": "4e9a34768e059c8cae8714515271568f",
+    "url": "/portfolio/static/media/reactjs.4e9a3476.png"
+  },
+  {
+    "revision": "6a2208d38b81f26eb5d6f40f4b0ec77e",
+    "url": "/portfolio/static/media/css.6a2208d3.png"
+  },
+  {
+    "revision": "bf151eeb8b27d79149353d40c247d81b",
+    "url": "/portfolio/static/media/ruby.bf151eeb.png"
+  },
+  {
+    "revision": "3d77bbbf9bf9b3f3c8eef6e9470dc43d",
+    "url": "/portfolio/static/media/rails.3d77bbbf.png"
+  },
+  {
+    "revision": "e5b392aea4368f3af585a6e5ed1c0725",
+    "url": "/portfolio/static/media/html.e5b392ae.png"
+  },
+  {
+    "revision": "28b9ceea00ec5b7862d5495f7f7e1e3c",
+    "url": "/portfolio/static/media/redux.28b9ceea.png"
+  },
+  {
+    "revision": "a04ea87132d8f102d33b8c2e4dd4009d",
+    "url": "/portfolio/static/media/postgresql.a04ea871.png"
+  },
+  {
+    "revision": "d2b1e19091a3530dfb8bec06b5a926dc",
+    "url": "/portfolio/static/media/mui.d2b1e190.png"
+  },
+  {
+    "revision": "6b234c34c642799cf95dc76269a3d1b6",
+    "url": "/portfolio/static/media/bootstrap.6b234c34.png"
+  },
+  {
+    "revision": "e0c2e25d2918fb58b67e0b74811e9aa3",
+    "url": "/portfolio/static/media/tailwind.e0c2e25d.png"
+  },
+  {
+    "revision": "0df3ec686a428a23814a918318942d92",
+    "url": "/portfolio/static/media/materialize.0df3ec68.png"
+  },
+  {
+    "revision": "0649e2840e9e6d1807237d77eddf8bca",
+    "url": "/portfolio/static/media/wineFanatic.0649e284.png"
+  },
+  {
+    "revision": "052894a7366997c0443896c766a5e543",
+    "url": "/portfolio/static/media/matchday.052894a7.png"
+  },
+  {
+    "revision": "ef6bbbafcc871b9387586808967b4e81",
+    "url": "/portfolio/static/media/wfblur2.ef6bbbaf.png"
+  },
+  {
+    "revision": "d88e283d5e9c7c3857b686863723711c",
+    "url": "/portfolio/static/media/mdblur2.d88e283d.png"
+  },
+  {
+    "revision": "a355a01dc46ec1d0ed6fed934635ff27",
+    "url": "/portfolio/index.html"
+  }
+];

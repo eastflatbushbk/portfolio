ed4bcef4c2b29059d6bf57553bac3674e3b38a1d
Robert Sainton <robertsainton@outlook.com>
2024-02-05 02:04:10 -0500

    changes route paths

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ function App() {
     <div>
       <Navbar/>
       <Routes>
-                          <Route exact path="/home" element={<Home />} />
+                          <Route exact path="/portfolio/home" element={<Home />} />
                           {/* <Route exact path="/about" element={<About />} /> */}
-                          <Route exact path="/skills" element={<Skills />} />
-                          <Route exact path="/projects" element={<Project   />} />
-                          <Route exact path="/project-one" element={<WineFanaticPage />} />
-                          <Route exact path="/project-two" element={<MatchdayPage  />} />                       
+                          <Route exact path="/portfolio/skills" element={<Skills />} />
+                          <Route exact path="/portfolio/projects" element={<Project   />} />
+                          <Route exact path="/portfolio/project-one" element={<WineFanaticPage />} />
+                          <Route exact path="/portfolio/project-two" element={<MatchdayPage  />} />                       
                     </Routes>
     </div>
     

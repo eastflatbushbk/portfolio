45bcd4e4e251fa1448d40d97ffff739723fc3137
Robert Sainton <robertsainton@outlook.com>
2024-02-06 13:40:24 -0500

    tested route 7

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React from 'react';
 import Navbar from './Navbar';
 import Home from './components/Home';
-import { Routes ,Route } from "react-router-dom";
+import { BrowserRouter, Routes ,Route } from "react-router-dom";
 import Skills from './components/Skills';
 import Project from './components/Project';
 import WineFanaticPage from './components/WineFanaticPage';
@@ -13,14 +13,16 @@ function App() {
   return (
     <div>
       <Navbar/>
-      <Routes basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename="/porfolio">
+            <Routes>
                           <Route exact path="/portfolio/home" element={<Home />} />
                           {/* <Route exact path="/about" element={<About />} /> */}
                           <Route exact path="/portfolio/skills" element={<Skills />} />
                           <Route exact path="/portfolio/projects" element={<Project   />} />
                           <Route exact path="/portfolio/project-one" element={<WineFanaticPage />} />
-                          <Route exact path="/portfolio/project-two" element={<MatchdayPage  />} />                       
-                    </Routes>
+                          <Route exact path="/portfolio/project-two" element={<MatchdayPage  />} />
+                         </Routes>                        
+                    </BrowserRouter>
                     <Home />
                     <Skills />
                     <Project   />
